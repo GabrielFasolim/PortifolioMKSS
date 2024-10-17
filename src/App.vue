5d949061185a342f9600125da550452612887ad6
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2024-10-17 20:39:32 -0300

    [add] tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <h1>teste</h1>
+    <h1 class="font-extrabold">teste</h1>
+    <el-button type="primary">Primary</el-button>
   </div>
  
 </template>
 
 <script setup lang="ts">
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 </script>
-<style scoped>
 
+<style scoped>
 
 </style>
