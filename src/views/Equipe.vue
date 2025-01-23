36756935009e3cb6d638b7996c101e31b221c8b6
fasolim <gabriel.fasolim@radioenge.com.br>
2025-01-23 09:34:49 -0300

    [add] pictures

--- src/views/Equipe.vue
+++ src/views/Equipe.vue
@@ -3,19 +3,59 @@
         <div class="text-3xl flex justify-center text-primary500 font-bold">
             <h1>Equipe</h1>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-10 justify-items-center mt-10">
-            <div v-for="(card, index) in cards" :key="index" class="flex flex-col items-center">
-                <div class="w-[200px] bg-primary h-[200px] rounded-full">
-                    <img :src="card.perfil" alt="Perfil" class="w-full h-full object-cover rounded-full">
-                </div>
-                <div class="mt-4 text-center">
-                    <h1 class="text-2xl font-bold text-primary500">{{card.title}}</h1>
-                    <p class="text-primary500 mb-2 mt-2">{{card.cargo}}</p>
-                    <div class="flex justify-center">
-                        <a :href="card.linkedin" target="_blank">
-                            <img class="w-[30px]" src="../assets/linkedinEquipe.png" alt="LinkedIn">
-                        </a>
+        <div class="flex flex-col items-center mt-10">
+            <div class="flex justify-center gap-[300px]">
+                <div class="flex flex-col items-center transition-transform duration-300 hover:scale-105">
+                    <div class="w-[200px] h-[200px] rounded-full">
+                        <img :src="cards[0].perfil" alt="Perfil" class="w-full h-full object-cover rounded-full">
                     </div>
+                    <h1 class="text-2xl font-bold text-primary500">{{cards[0].title}}</h1>
+                    <p class="text-primary500 mb-2 mt-2">{{cards[0].cargo}}</p>
+                    <a :href="cards[0].linkedin" target="_blank">
+                        <img class="w-[30px]" src="../assets/linkedinEquipe.png" alt="LinkedIn">
+                    </a>
+                </div>
+                <div class="flex flex-col items-center transition-transform duration-300 hover:scale-105">
+                    <div class="w-[200px] h-[200px] rounded-full">
+                        <img :src="cards[1].perfil" alt="Perfil" class="w-full h-full object-cover rounded-full">
+                    </div>
+                    <h1 class="text-2xl font-bold text-primary500">{{cards[1].title}}</h1>
+                    <p class="text-primary500 mb-2 mt-2">{{cards[1].cargo}}</p>
+                    <a :href="cards[1].linkedin" target="_blank">
+                        <img class="w-[30px]" src="../assets/linkedinEquipe.png" alt="LinkedIn">
+                    </a>
+                </div>
+                <div class="flex flex-col items-center transition-transform duration-300 hover:scale-105">
+                    <div class="w-[200px] h-[200px] rounded-full">
+                        <img :src="cards[2].perfil" alt="Perfil" class="w-full h-full object-cover rounded-full">
+                    </div>
+                    <h1 class="text-2xl font-bold text-primary500">{{cards[2].title}}</h1>
+                    <p class="text-primary500 mb-2 mt-2">{{cards[2].cargo}}</p>
+                    <a :href="cards[2].linkedin" target="_blank">
+                        <img class="w-[30px]" src="../assets/linkedinEquipe.png" alt="LinkedIn">
+                    </a>
+                </div>
+            </div>
+            <div class="flex justify-center mt-10 gap-[300px]">
+                <div class="flex flex-col items-center transition-transform duration-300 hover:scale-105">
+                    <div class="w-[200px] h-[200px] rounded-full">
+                        <img :src="cards[3].perfil" alt="Perfil" class="w-full h-full object-cover rounded-full">
+                    </div>
+                    <h1 class="text-2xl font-bold text-primary500">{{cards[3].title}}</h1>
+                    <p class="text-primary500 mb-2 mt-2">{{cards[3].cargo}}</p>
+                    <a :href="cards[3].linkedin" target="_blank">
+                        <img class="w-[30px]" src="../assets/linkedinEquipe.png" alt="LinkedIn">
+                    </a>
+                </div>
+                <div class="flex flex-col items-center transition-transform duration-300 hover:scale-105">
+                    <div class="w-[200px] h-[200px] rounded-full">
+                        <img :src="cards[4].perfil" alt="Perfil" class="w-full h-full object-cover rounded-full">
+                    </div>
+                    <h1 class="text-2xl font-bold text-primary500">{{cards[4].title}}</h1>
+                    <p class="text-primary500 mb-2 mt-2">{{cards[4].cargo}}</p>
+                    <a :href="cards[4].linkedin" target="_blank">
+                        <img class="w-[30px]" src="../assets/linkedinEquipe.png" alt="LinkedIn">
+                    </a>
                 </div>
             </div>
         </div>
@@ -45,7 +85,7 @@ export default {
                     title: "Carlos Scorsoni",
                     cargo: "Desenvolvedor Back-end",
                     linkedin: "https://www.linkedin.com/in/carlos-eduardo-scorsoni-dias-825918236/",
-                    perfil: "../../src/assets/carlos.png",
+                    perfil: "../../src/assets/kadu.png",
                 },
                 {
                     title: "Gabriel Fasolim",
@@ -57,7 +97,7 @@ export default {
                     title: "Gustavo Bordignon",
                     cargo: "Desenvolvedor Front-end",
                     linkedin: "https://www.linkedin.com/in/gustavo-bordignon-326861203/",
-                    perfil: "../../src/assets/gustavo.png",
+                    perfil: "../../src/assets/gb.png",
                 }
             ],
         };
@@ -79,11 +119,10 @@ export default {
         grid-template-rows: auto auto;
         grid-template-columns: repeat(3, 1fr);
     }
-    .grid > :nth-child(4) {
-        grid-column: 2 / span 1;
-    }
+    .grid > :nth-child(4),
     .grid > :nth-child(5) {
-        grid-column: 3 / span 1;
+        grid-column: span 1;
+        justify-self: center;
     }
 }
 
