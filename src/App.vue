c9afd9d7f2c26574afce9279c97265348e8e3f60
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2025-01-28 21:55:28 -0300

    feat: add mobile sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,31 @@
 <template>
-  <Header/>
-  <Apresentation/>
-  <Projects/>
-  <About/>
-  <Video/>
-  <Equipe/>
-
-  
+  <Header />
+  <Apresentation />
+  <Projects />
+  <About />
+  <Video />
+  <Equipe />
 </template>
 
 <script lang="ts">
-import Header from './views/Header.vue'
-import Apresentation from './views/Apresentation.vue'
-import Projects from './views/Projects.vue'
-import About from './views/About.vue';
-import Video from './views/Video.vue';
-import Equipe from './views/Equipe.vue';
-
-
+import Header from "./views/Header.vue";
+import Apresentation from "./views/Apresentation.vue";
+import Projects from "./views/Projects.vue";
+import About from "./views/About.vue";
+import Video from "./views/Video.vue";
+import Equipe from "./views/Equipe.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
-    Apresentation,
-    Projects,
-    About,
-    Video,
-    Equipe
-  }
-}
+    // Apresentation,
+    //Projects,
+    //About,
+    //Video,
+    //Equipe,
+  },
+};
 </script>
 
 <style scoped></style>
