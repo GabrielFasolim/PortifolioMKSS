4cbd4f33fb97a3740af557d8002a3cfdadda4299
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2025-02-02 20:44:24 -0300

    fix: font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,32 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        background: '#F7F7F9',
-        primary: '#002285',
-        primary900: '#00060F',
-        primary800: '#000D2D',
-        primary700: '#00144A',
-        primary600: '#001B68',
-        primary500: '#002285',// Azul escuro personalizado
-        primary400: '#4A59E5', // Azul claro personalizado
-        primary300: '#4A59E5',
-        primary200: '#A4ACF0',
-        primary100: '#D1D6F5',
-       
+        background: "#F7F7F9",
+        primary: "#002285",
+        primary900: "#00060F",
+        primary800: "#000D2D",
+        primary700: "#00144A",
+        primary600: "#001B68",
+        primary500: "#002285", // Azul escuro personalizado
+        primary400: "#4A59E5", // Azul claro personalizado
+        primary300: "#4A59E5",
+        primary200: "#A4ACF0",
+        primary100: "#D1D6F5",
       },
       fontFamily: {
-        outfit: ['Outfit', 'sans-serif'], // Adiciona a fonte Outfit
+        outfit: ["Outfit", "sans-serif"], // Adiciona a fonte Outfit
       },
       fontSize: {
-        h1: '40px',
-        h2: '36px',
-        h3: '32px',
-        big: '24px',
-        medium: '20px',
-        default: '16px',
-        small: '12px',
+        h1: "40px",
+        h2: "36px",
+        h3: "32px",
+        big: "24px",
+        medium: "20px",
+        default: "16px",
+        small: "12px",
       },
       fontWeight: {
         light: 300,
@@ -42,4 +41,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
